05d39cb3db7461f26f01bdb97fcbe6fdbf8dabbe
Muhammad Irvandi Lubis <113462342+muhammadirvandilubis@users.noreply.github.com>
2022-12-07 21:40:30 +0700

    Add files via upload

---
+++ pengembalian-service/src/main/java/com/vandi/pengembalian/PengembalianServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vandi.pengembalian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PengembalianServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PengembalianServiceApplication.class, args);
+	}
+
+}
